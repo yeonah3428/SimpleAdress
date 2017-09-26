789f462f0eb8c63674770a503ddaef598f701fcc
D7703_22 <d7703_22@D7703-22ui-iMac.local>
2017-09-26 11:12:19 +0900

    Initial Commit

---
+++ 01_Simplecode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01_Simplecode
+//
+//  Created by D7703_22 on 2017. 9. 26..
+//  Copyright © 2017년 D7703_22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
